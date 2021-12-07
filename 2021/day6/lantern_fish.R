5760695e092e1e9a961b1067d084497e3a35ba16
Brian Holland <bhollan@users.noreply.github.com>
2021-12-07 16:42:27 -0600

    Day 6 complete and Day 7 setup

--- 2021/day6/lantern_fish.R
+++ 2021/day6/lantern_fish.R
@@ -1,19 +1,32 @@
-library(dplyr)
-library(tidyr)
-library(stringr)
+# library(dplyr)
+# library(tidyr)
+# library(stringr)
 # library(RcppRoll)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-
-fileName <- "input1.txt"
-# fileName <- "input2.txt"
+# fileName <- "input1.txt"
+fileName <- "input2.txt"
 
 contents <- readChar(fileName, file.info(fileName)$size)
-fish <- as.numeric(unlist(str_split(contents, ",")))
-#Take example and "hash" it out to create the "correct" mappings for the first 18 days
+fish_days <- as.numeric(unlist(str_split(contents, ",")))
+#Example"hashed" out to create the "correct" mappings for the first 18 days: input1_as_tables.txt
+#R is 1-indexed so day "0" fish are actually day "1" fish here
 
-#allocate quantities to vector slots
-# every day: 
-#   create new "eights"
-#   add "zeros" to "six" (and clear)
+fish <- as.numeric(c())
+for(i in 1:9){
+  fish[i] <- sum(fish_days == (i-1))
+}
+
+age_fish_by <- function(fish, n){
+  for(i in 1:n){
+    fish <- c(fish[-(1)], fish[1])
+    fish[7] <- fish[7] + fish[9]
+    print(fish)
+  }
+  return(fish)
+}
+
+fish <- sum(age_fish_by(fish, 256))
+#options(scipen = 100) sets the R session's scientific notation to show us the whole number
+print(fish)
